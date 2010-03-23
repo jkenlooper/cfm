e4d20c86964bdcee83624a218c5dbd0a11787834
jake <jake@b40079c4-dec9-49e4-8aa6-4d8200c7a0a2>
2010-03-23 11:42:00 +0000

    initial Controller methods
    git-svn-id: svn+ssh://wag1/home/svn/repos/cloudfile/trunk@7 b40079c4-dec9-49e4-8aa6-4d8200c7a0a2

--- cloudfile.py
+++ cloudfile.py
@@ -34,13 +34,41 @@ class File(object):
     return locals()
 
 class Controller(object):
-  """ a Controller """
-  def __init__(self, owner_name, login_name, api_key):
-    #TODO: set these
+  """ a Controller that works in a single directory """
+  def __init__(self, owner_name, login_name, api_key, directory):
+    self.owner_name = owner_name
+    self.directory = directory
+    #TODO: get a connection to the cloud using the api_key and login_name
+  
+  @Property
+  def directory():
+    doc = "directory to operate in"
+    def fget(self):
+      return self._directory
+    def fset(self, dir):
+      #TODO: check to see if the dir exists and is a directory
+      self._directory = dir
+    return locals()
+
+  def add_files(self, container_name, file_list):
+    """ add all the files in file_list and create a cloudfile.yaml for each. """
+    pass
+  def get_new(self):
+    """ search for cloudfile.yaml files and compare the hashes and download new if different or not existant. """
+    pass
+  def update(self):
+    """ Update any that are different. Any that are different and have a different owner; get a copy of the one on server and add "owner_name." in front of it. Any that no longer exist in the cloud rename the file with "deleted." in front of it. """
+    pass
+  def clean(self):
+    """ delete files just from local directory and leave the yaml untouched """
+    pass
+  def delete_files(self, file_list):
+    """ Remove files from cloud and local directory """
+    pass
+  def steal_files(self, file_list):
+    """ any files that are different and have a different owner; replace file in cloud and change owner. """
     pass
 
-  def add_files(container_name, file_list):
-    pass
 
 if __name__ == "__main__":
   parser = OptionParser(usage="%%prog --action [%s] [options] [files and or directories]" % "|".join(ACTIONS), version="%prog 0.1", description="add files to the cloud")
@@ -50,13 +78,17 @@ if __name__ == "__main__":
       type="choice",
       choices=ACTIONS,
       help="Specify what action to do with the files/directories listed. choices: %s" % ", ".join(ACTIONS))
-  parser.add_option("--config", "-c",
+  parser.add_option("--config",
       action="store",
       type="string",
       help="specify a different config file other then the default ~.cloudfile.config one.")
   parser.add_option("--recursive", "-R",
       action="store_true",
       help="For any directories listed in args find all the *.cloudfile.yaml")
+  parser.container("--container", "-c",
+      action="store",
+      type="string",
+      help="Set the name of the container to work in")
 
 
   (options, args) = parser.parse_args()
@@ -68,7 +100,4 @@ if __name__ == "__main__":
     parser.error("Must specify an action")
   elif options.action == ACTION_ADD and not options.container:
     parser.error("Must set a container name when adding files")
-
-
-
 
