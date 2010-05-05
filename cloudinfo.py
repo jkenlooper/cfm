fe0478b5b8cd40291c7698a12b58d2c72c305546
jake <jake@b40079c4-dec9-49e4-8aa6-4d8200c7a0a2>
2010-05-05 01:54:56 +0000

    added cloudinfo.py script
    git-svn-id: svn+ssh://wag1/home/svn/repos/cloudfile/trunk@32 b40079c4-dec9-49e4-8aa6-4d8200c7a0a2

---
+++ cloudinfo.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+'''cloud info script'''
+
+import cloudfiles
+import ConfigParser
+from getpass import getuser
+from optparse import OptionParser
+
+if __name__ == "__main__":
+  parser = OptionParser(version="%prog 0.1", description="show general info on the cloud")
+  parser.add_option("--verbose", "-v",
+      action="store_true",
+      help="show all objects in the container")
+  parser.add_option("--config",
+      action="store",
+      type="string",
+      default="/home/%s/cloudfile.cfg" % getuser(), # better way of doing this?
+      help="specify a cloud connection config file.")
+  parser.add_option("--container", "-c",
+      action="store",
+      type="string",
+      help="show info on just this container")
+
+  (options, args) = parser.parse_args()
+
+  config = ConfigParser.SafeConfigParser()
+  config.read(options.config)
+  conn = cloudfiles.get_connection(config.get('server', 'login_name'), config.get('server', 'api_key'), servicenet=config.getboolean('server', 'servicenet'))
+
+  if options.container:
+    containers = [conn.get_container(options.container),]
+  else:
+    containers = conn.get_all_containers()
+
+  for container in containers:
+    out = container.name
+    if container.is_public():
+      out = "%s %s" % (out, container.public_uri())
+    try:
+      if options.verbose:
+        #c = conn.get_container(container.name)
+        c = container
+        objects = c.get_objects()
+        obj_names = []
+        for storage_object in objects:
+          obj_names.append(storage_object.name)
+        out = "%s : \n %s" % (out, "  ".join(obj_names))
+
+    except NoSuchContainer:
+      print "no such container: %s" % container.name
+
+    print out
